63d12af88ddd0f522d31514dd2c024eae9d9e824
Meijuta <meijuta@proton.me>
2025-04-30 22:18:25 +1000

    circle oms

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,110 @@
+// https://learnopengl.com/Getting-started/Hello-Window
+
+#include <cmath>
 #include <iostream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "renderer.hpp"
+#include "shader_s.hpp"
+
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+
+  // initialise GLFW
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+  // initialise GLFW window
+  GLFWwindow *window = glfwCreateWindow(512, 512, "glThings", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  // initialise GLAD function pointers
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
 
 
 
+  //// SHADERS ////
+  Shader ourShader("./shader.vert", "./shader.frag");
+  
+  
+  
+  renderer::colour backgroundColour;
+  backgroundColour.tcolour[0] = 0.4f;
+  backgroundColour.tcolour[1] = 0.3f;
+  backgroundColour.tcolour[2] = 0.2f;
+
+  renderer::circle tmpCircle;
+  tmpCircle.origin[0] = 0.0f;
+  tmpCircle.origin[1] = 0.0f;
+  tmpCircle.radius = 0.5f;
+
+
+  
+  
+  
+  
+
+  renderer::init();
+
+  // uncomment this call to draw in wireframe polygons.
+  //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+  while (!glfwWindowShouldClose(window)) {
+
+    processInput(window);
+
+    glClearColor(
+      /*
+      (sin(glfwGetTime() + (3.14f / 3 * 3)) + 1.0f) / 2.0f, // red
+      (sin(glfwGetTime() + (3.14f / 3 * 2)) + 1.0f) / 2.0f, // green
+      (sin(glfwGetTime() + (3.14f / 3 * 1)) + 1.0f) / 2.0f, // blue
+      */
+      0.80f, 0.40f, 0.20f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    
+    render::circle(tmpCircle, backgroundColour);
+
+    
+    
+    ourShader.use();
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+  }
+
+
+  /*
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
+  */
+  glfwTerminate();
+  return 0;
+}
