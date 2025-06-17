9aec8ad6e77cba0ce3203edefbd3cc6c1d8122cd
Marcel-de-Jong-Education <marcel.dejong546@education.nsw.gov.au>
2025-06-17 14:20:41 +1000

    Updated gravity to realistically describe inertia

--- src/astrophysics.hpp
+++ src/astrophysics.hpp
@@ -127,8 +127,12 @@ namespace celestial //
                     std::cout <<  "WARNING: DIVISION BY ZERO!\n\n"; // this is extremely rare so if it happens itd be cool to know about it!!
                     return std::vector<double>{0,0,0}; // just return 0 and avoid nonsense
                 }
-                double net_force = G * (mass * target.mass) / (distance*distance); // G(m1*m2)/(d^2); the gravitational force felt by BOTH objects COMBINED
-                double self_force = net_force * target.mass / (mass+target.mass); // how much of the force THIS object perceives
+		// G*m_1*m_2 / d^2
+                double force = G * (mass * target.mass) / (distance*distance); 
+		// F = ma
+		// a = F/m
+                double self_force = force / mass; // account for inertia
+		    
                 double dx = target.pos[0]-pos[0]; // x1 - x2
                 double dy = target.pos[1]-pos[1]; // y1 - y2
 
